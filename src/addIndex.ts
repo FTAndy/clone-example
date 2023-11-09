9c111e74958ad00f1366cd5e308224b236dbbcfe
FTAndy <fortheandy@gmail.com>
2023-11-09 11:33:36 +0800

    chore(index): remove unnecessary console log

--- src/addIndex.ts
+++ src/addIndex.ts
@@ -13,8 +13,6 @@ async function reset() {
   await Comedian.createIndex({ name: "text" })
   await Special.createIndex({ name: 'text'})
 
-  console.log('sdsds')
-
   // const cursor = CrawlerTask.find<CarwlerTask>({})
 
   await dbClient.close()
