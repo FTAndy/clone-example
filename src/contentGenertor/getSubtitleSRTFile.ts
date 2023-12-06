6f297c197d3fc7c6ef2e65babfea2e260bfbd40f
FTAndy <fortheandy@gmail.com>
2023-12-06 11:51:43 +0800

    feat: refactor getSubtitleSRTFile function and add axios dependency
    - Add `axios` package as a dependency
    - Import `axios`, `fs`, and `path` in `getSubtitleSRTFile.ts`
    - Define `os` variable in `getSubtitleSRTFile.ts`
    - Move the initialization of `os` to a separate `initOS` function in `getSubtitleSRTFile.ts`
    - Pass `comedianName` parameter to `getSubtitleSRTFile` function in `getSubtitleSRTFile.ts`
    - Adjust the query name to `specialName` in `getSubtitleSRTFile` function in `getSubtitleSRTFile.ts`
    - Fix the path of `srtFile` in `getSubtitleSRTFile` function in `getSubtitleSRTFile.ts`
    - Fix the code indentation in `getSubtitleSRTFile`
    - Add a new `main` function and call `getSubtitleSRTFile` multiple times using `maxLimitedAsync`
    
    Signed-off-by: FTAndy <fortheandy@gmail.com>

--- src/contentGenertor/getSubtitleSRTFile.ts
+++ src/contentGenertor/getSubtitleSRTFile.ts
@@ -1,26 +1,42 @@
 import 'dotenv/config'
 import 'global-agent/bootstrap';
+import axios from 'axios'
+import fs from 'fs'
+import path from 'path'
+import { maxLimitedAsync } from '../utils/maxLimitedAsync'
 const OpenSubtitles = require('opensubtitles-node-sdk');
 
-export default async function getSubtitleSRTFile(specialName: string, format: string = 'srt') {
-  console.log(process.env.OPEN_SUBTITLE, 'process.env.OPEN_SUBTITLE')
-  const os = new OpenSubtitles({
+
+let os: any
+
+async function initOS() {
+  os = new OpenSubtitles({
     apikey: process.env.OPEN_SUBTITLE,
     headers: {
       'User-Agent': 'family v0.1'
     }
   })
 
-  const passwordRes = await os.login({
+  await os.login({
     username: process.env.OPEN_SUBTITLE_USERNAME,
     password: process.env.OPEN_SUBTITLE_PASSWORD
   })
+}
+
+export default async function getSubtitleSRTFile(specialName: string, comedianName: string, format: string = 'srt') {
+  if (!os) {
+    await initOS()
+  }
+
+  const queryName = specialName
 
   const response = await os.subtitles({
-    query: 'Dave Chappelle: Stick and Stones',
+    query: queryName
   })
 
-  console.log(response.data[0].attributes.files)
+  console.log(response, 'response', queryName)
+
+  console.log(response?.data[0]?.attributes?.files)
 
   const fileId = response?.data[0]?.attributes?.files[0]?.file_id
 
@@ -29,10 +45,50 @@ export default async function getSubtitleSRTFile(specialName: string, format: st
       file_id: fileId
     })
 
-    console.log(downloadResponse, 'downloadResponse')
+    const link = downloadResponse?.link
+    if (link) {
+      const response = await axios({
+        method: 'get',
+        url: link,
+        responseType: 'stream'
+      })
+      const srtFile = path.resolve(
+        __dirname,
+        '../../',
+        'temp',
+        // trimSpecial(`${comedianName}-${specialName}-${subtitle.lan}.srt`),
+        specialName + '.srt'
+      );
+
+      response.data.pipe(fs.createWriteStream(srtFile));
+    }
     
   }
 
 }
 
-getSubtitleSRTFile('Dave Chappelle: Stick and Stones')
+async function main () {
+  const specialList = [
+    "Epilogue: The Punchline",
+    "Dave Chappelle: What's in a Name?",
+    "Dave Chappelle: The Closer",
+    "Dave Chappelle: The Bird Revelation",
+    "The Age of Spin: Dave Chappelle Live at the Hollywood Palladium",
+    "Dave Chappelle: Sticks & Stones",
+    "Deep in the Heart of Texas",
+    "Dave Chappelle: Equanimity",
+    "Dave Chappelle: For What It's Worth",
+    "Dave Chappelle Unforgiven",
+    "Dave Chappelle: 8:46",
+    "Dave Chappelle: Killin' Them Softly"
+  ]
+
+  await maxLimitedAsync({
+    max: 3,
+    tasks: specialList.map(specialName => {
+      return () => getSubtitleSRTFile(specialName, 'Dave Chapplle')
+    })
+  })
+}
+
+main()
